f7954b84aef9e0a3353c6ab58894c89e8f3ec147
JMasip <jordi@masip.cat>
2014-03-08 12:52:31 +0100

    added getArchitecture(), improved getLibs() and getBetween()

--- magic_methods.py
+++ magic_methods.py
@@ -11,11 +11,40 @@ def read_file(filename):
 
 def getBetween(s, pref, suf):
 	try:
-		start = s.index(pref) # if start != 0 else 0
-		end = s[start:].index(suf)
+		if pref == "":
+			start = 0
+		else:
+			start = s.index(pref) # if start != 0 else 0
+		if suf == "":
+			end = len(s)
+		else:
+			end = s[start:].index(suf)
 		return (s[start + len(pref):start+end], start+end)
 	except Exception:
 		return ("", -1)
+
+def getLibs(vhdl_file):
+	libs = {}
+	value = ("", 0)
+	while True:
+		value = getBetween(vhdl_file[value[1]:], "library", ";")
+		if value[1] == -1:
+			break
+		lib_name = value[0].strip().lower()
+		libs[lib_name] = Library(lib_name)
+	value = ("", 0)
+	while True:
+		value = getBetween(vhdl_file[value[1]:], "use", ";")
+		if value == ("", -1):
+			break
+		use_statment = value[0].strip().lower().split(".")
+		lib, package = use_statment[0], ".".join(use_statment[1:])
+		if lib in libs.keys():
+			libs[lib].addPackage(package)
+		else:
+			print "ERR: Using library '" + lib + "' in package '" + lib + "." + package + ";' without adding the library"
+			break
+	return libs.values()
 
 def getEntities(vhdl_file):
 	entities = []
@@ -27,9 +56,8 @@ def getEntities(vhdl_file):
 			break
 		between_entity = getBetween(vhdl_file, entity.getName() + " is", "end")[0].strip()
 		port = ""
-		isPortFound = False			
 		bracket_counter = 0
-		isCounting = False
+		isCounting, isPortFound = False, False
 		for i in range(len(between_entity)):
 			if between_entity[i:i+4] == "port":
 				isCounting = True
@@ -51,23 +79,41 @@ def getEntities(vhdl_file):
 		entities += [entity]
 	return entities
 
-def getLibs(vhdl_file):
-	libs = {}
-	value = ("", 0)
-	i = 0
-	while True:
-		value = getBetween(vhdl_file[value[1]:], "library", ";")
-		if value[1] == -1:
-			break
-		lib_name = value[0].strip().lower()
-		libs[lib_name] = Library(lib_name)
+def getArchitecture(vhdl_file, entity):
+	entities = []
 	value = ("", 0)
 	while True:
-		value = getBetween(vhdl_file[value[1]:], "use", ";")
-		if value[1] == -1:
+		value = getBetween(vhdl_file[value[1]:], "architecture ", " is")
+		arch_name = getBetween(value[0], "", " of")[0].strip()
+		ent_name = getBetween(value[0], "of ", "")[0].strip()
+		if arch_name == "" or ent_name == "":
 			break
-		use_statment = value[0].strip().lower().split(".")
-		lib, package = use_statment[0], ".".join(use_statment[1:])
-		if lib in libs.keys():
-			libs[lib].addPackage(package)
-	return libs.values()
+		if ent_name != entity.getName():
+			continue
+		return Architecture(arch_name, entity)
+		"""if value == ("", -1) or entity in entities:
+			break
+		between_entity = getBetween(vhdl_file, entity.getName() + " is", "end")[0].strip()
+		port = ""
+		bracket_counter = 0
+		isCounting, isPortFound = False, False
+		for i in range(len(between_entity)):
+			if between_entity[i:i+4] == "port":
+				isCounting = True
+			if isCounting:
+				port += between_entity[i]
+				if between_entity[i] == "(":
+					bracket_counter += 1
+				elif between_entity[i] == ")" or between_entity[i:i+1] == ");":
+					bracket_counter -= 1
+					if bracket_counter == 0:
+						port += ";"
+						isPortFound = True
+						break
+		else:
+			isPortFound = False
+		
+		if isPortFound:
+			entity.setPort(port)
+		entities += [entity]
+	return entities"""
