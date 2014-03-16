c8eec98f99052e00197dcc823eded3b321d66b1d
felipe42 <felipe42x@gmail.com>
2014-03-16 13:43:44 +0100

    -

--- tb_gen.py
+++ tb_gen.py
@@ -2,30 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 from vhdl import *
 from vParser import *
 
-# Used to test code using SublimeText
-sys.argv += ["tests/HVcounter.vhd"]
+name=sys.argv[1]
+
 
 if len(sys.argv) != 2:
-	print "Filename not specified"
+	print "Especifica nom del fitxer"
 	sys.exit(1)
 
-# Get VHDL file from arguments
+
+if '.vhd' not in name:
+	print 'Extenció no vàlida'
+	sys.exit(1)
+
+
+maketb=name.split('.')
+maketb=maketb[0]+'_tb.'+maketb[1]
+tb_result = ""
+
 vhd_file = sys.argv[1]
 
-# Read VHDL file
 vhd_file = read_file(vhd_file).lower()
 
-# Create a new VHDL object
 vhdl = VHDL()
 
-# Add libraries and packages to 'vhdl'
 for l in getLibs(vhd_file):
 	vhdl.addLibrary(l)
 
-# Add entities to 'vhdl'
 for entity in getEntities(vhd_file):
 	vhdl.setEntity(entity)
 
@@ -35,22 +41,115 @@ for entity in vhdl.getEntities():
 	if arch != "":
 		vhdl.setArchitecture(arch)
 
-# Get each lib in this file:
-for lib in vhdl.getLibs():
-	print lib
 
-# Get each arch in vhdl file
-for architecture in vhdl.getArchitectures():
-	print architecture
+print """
+dP     dP dP     dP  888888ba  dP        
+88     88 88     88  88    `8b 88        
+88    .8P 88aaaaa88a 88     88 88        
+88    d8' 88     88  88     88 88        
+88  .d8P  88     88  88    .8P 88        
+888888'   dP     dP  8888888P  88888888P 
+ooooooooooooooooooooooooooooooooooooooooo
+                                         
+d888888P  888888ba   .88888.                    
+   88     88    `8b d8'   `88                   
+   88    a88aaaa8P' 88        .d8888b. 88d888b. 
+   88     88   `8b. 88   YP88 88ooood8 88'  `88 
+   88     88    .88 Y8.   .88 88.  ... 88    88 
+   dP     88888888P  `88888'  `88888P' dP    dP 
+oooooooooooooooooooooooooooooooooooooooooooooooo
 
-	# Get each signal in arch
-	for signal in architecture.getSignalList().values():
-		print "\t", signal
+version: 1.0
+author: Felipe Arango, Jordi Masip
+
+"""
+
 	
-	# Get the entity of arch
-	entity = architecture.getEntity()
-	print "\t", entity
+def LibrarysTb(): #Librerias
+	result = ""
+	libss=[]
+	use=[]
+	for lib in vhdl.getLibs():
+		result += 'library '+lib.getName()+';\n'
+	
+	for lib in vhdl.getLibs():
+		for pack in lib.getPackages(): 
+			result += "use " + pack+';\n'
+	return result + "\n"
+			
+def entityTb(): #Entidad
+	result = ""
+	for architecture in vhdl.getArchitectures():
+		entity = architecture.getEntity()		#Puede ser global?
+		result += 'entity '+entity.getName()+'_tb is'+'\n'+'end '+entity.getName()+'_tb;\n'
+	return result + "\n"
 
-	# Print each port defined in entity statment
-	for port in entity.getPorts().values():
-		print "\t\t", port
+def architectureTb():
+	result = ""
+	for architecture in vhdl.getArchitectures():
+		entity = architecture.getEntity()
+	result += 'architecture behav of '+entity.getName()+'_tb is\n'+'\tcomponent my_'+entity.getName()+'\n'
+	return result
+
+def portsTb():
+	result = '\tport ('
+	for architecture in vhdl.getArchitectures():
+		entity = architecture.getEntity()
+		for i, port in enumerate(entity.getPorts().values()):
+			if i != 0:
+				result += '\t\t'
+			result += '\t'+port.getName()+' : '+port.getPortType()+' '+port.getType()+';\n'
+			if i == len(entity.getPorts().values())-1:
+				result = result[:-2] + ");\n"
+		result += '\tend component;'
+	return result
+
+def dutSignalsTb():
+	result = ""
+	for architecture in vhdl.getArchitectures():
+		entity = architecture.getEntity()
+		result += '\n\tfor dut : my_'+entity.getName()+' use entity work.'+entity.getName()+';\n'
+		for port in entity.getPorts().values():
+			result += '\tsignal t_'+port.getName()+' : '+port.getType()+';\n'
+		result += 'begin\n'
+	return result
+		
+def dutTb():
+	result = ""
+	b=0
+	for architecture in vhdl.getArchitectures():
+		entity = architecture.getEntity()
+		result += '\tdut: my_'+entity.getName()+'port map (\n'
+		for port in entity.getPorts().values():
+			if b != len(entity.getPorts().values())-1:
+				result += '\t\t'+port.getName()+'\t=> t_'+port.getName()+',\n'
+				b+=1
+			else:
+				result += '\t\t'+port.getName()+'\t=> t_'+port.getName()+');\n'
+	return result
+
+def clocktb():
+	result=""
+	res='sn'
+	while True: 
+		clk = raw_input('Vols un clock?: [s/n]')
+		if clk != 's' and clk != 'n':
+			print clk
+			print 'Opció no vàlida'
+		elif clk == 's':
+			result+="\tclk_process: process\n\tbegin\n\t\tclk <= '0';\n\t\twait for ¿? ns;\n\t\tfor i in 1 to ¿? loop\n\t\t\tclk <= not clk;\n\t\t\twait for ¿? ns;\n\t\tend loop;\n\t\twait;\n\t end process clk_process;"
+			return result
+		else:
+			return result
+			
+tb_result += LibrarysTb()
+tb_result += entityTb()
+tb_result += architectureTb()
+tb_result += portsTb()
+tb_result += dutSignalsTb()
+tb_result += dutTb()
+tb_result += clocktb()
+
+# Write to file
+write_file(maketb, tb_result)
+print maketb+' creat'
