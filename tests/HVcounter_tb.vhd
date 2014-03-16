342ac12a180323f02d60256d8abb70514a7276a0
JMasip <jordi@masip.cat>
2014-03-16 20:35:12 +0100

    Fixed testbench

--- tests/HVcounter_tb.vhd
+++ tests/HVcounter_tb.vhd
@@ -9,14 +9,26 @@ architecture behav of hvcounter_tb is
 	component my_hvcounter
 	port (	h : out std_logic_vector (9 downto 0);
 			v : out std_logic_vector (9 downto 0);
-			clk : in std_logic;
+			clk : in std_logic);
 	end component;
 	for dut : my_hvcounter use entity work.hvcounter;
 	signal t_h : std_logic_vector (9 downto 0);
 	signal t_v : std_logic_vector (9 downto 0);
 	signal t_clk : std_logic;
 begin
-	dut: my_hvcounterport map (
+	dut: my_hvcounter port map (
 		h	=> t_h,
 		v	=> t_v,
 		clk	=> t_clk);
+	clk_process: process
+	begin
+		t_clk <= '0';
+		wait for 0.00000000005000 ns;
+		for i in 1 to 100 loop
+			t_clk <= not t_clk;
+			wait for 0.00000000005000 ns;
+		end loop;
+		wait;
+	end process clk_process;
+	-- Els teus process van aqui:
+end behav;
